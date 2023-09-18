ee0ed5c0b718a0fb9e9a853ee78221a69d138a65
stastnan <nik.stastna@gmail.com>
2023-09-18 16:54:30 +0200

    Redux basic setup

--- src/features/movies/MovieDetails.jsx
+++ src/features/movies/MovieDetails.jsx
@@ -11,13 +11,15 @@ import {
   Text,
 } from "@chakra-ui/react";
 import { Link, useParams } from "react-router-dom";
-import { data } from "./data";
 import { MOVIEDB_IMAGES_URL } from "src/common/constants";
 import { ArrowBackIcon } from "@chakra-ui/icons";
+import { useSelector } from "react-redux";
+import { selectMovieById } from "./moviesSlice";
 
 function MovieDetails() {
   const { movieId } = useParams();
-  const movie = data.results.find((movie) => movie.id === Number(movieId));
+  const movie = useSelector((state) => selectMovieById(state, movieId));
+
   return (
     <Box minH="100vh">
       <Link to="/">
