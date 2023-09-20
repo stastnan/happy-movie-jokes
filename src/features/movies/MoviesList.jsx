78b3fc65dee05cd2752c7c74df1e92118c38be71
stastnan <nik.stastna@gmail.com>
2023-09-20 16:05:26 +0200

    Added movies fetching with async Thunk

--- src/features/movies/MoviesList.jsx
+++ src/features/movies/MoviesList.jsx
@@ -1,16 +1,43 @@
-import { useSelector } from "react-redux";
-import { Heading, SimpleGrid } from "@chakra-ui/react";
+import { useDispatch, useSelector } from "react-redux";
+import { Flex, Heading, SimpleGrid, Spinner, useToast } from "@chakra-ui/react";
 import { MOVIEDB_IMAGES_URL } from "src/common/constants";
 import MovieCard from "./MovieCard";
-import { selectAllMovies } from "./moviesSlice";
+import {
+  fetchMovies,
+  selectAllMovies,
+  selectMoviesError,
+  selectMoviesStatus,
+} from "./moviesSlice";
+import { useEffect } from "react";
 
 function MoviesList() {
   const movies = useSelector(selectAllMovies);
-  return (
-    <>
-      <Heading textAlign="center" size="xl" mb={4}>
-        Trending Movies
-      </Heading>
+  const moviesStatus = useSelector(selectMoviesStatus);
+  const moviesError = useSelector(selectMoviesError);
+  const dispatch = useDispatch();
+  const toast = useToast();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        await dispatch(fetchMovies()).unwrap();
+      } catch (err) {
+        toast({
+          title: "Failed to load movies",
+          description:
+            "Please, refresh the pate and check your internet connection",
+          status: "error",
+          duration: 9000,
+          isClosable: true,
+        });
+      }
+    };
+    fetchData();
+  }, [dispatch, toast]);
+
+  let content;
+  if (moviesStatus === "succeeded") {
+    content = (
       <SimpleGrid spacing={4} columns={{ sm: 1, md: 3, lg: 4 }}>
         {movies.map((movie) => (
           <MovieCard
@@ -22,6 +49,27 @@ function MoviesList() {
           />
         ))}
       </SimpleGrid>
+    );
+  } else if (moviesStatus === "loading") {
+    content = (
+      <Flex alignItems="center" justifyContent="center" minH="100vh">
+        <Spinner text="Loading..." />
+      </Flex>
+    );
+  } else if (moviesStatus === "failed") {
+    content = (
+      <Flex alignItems="center" justifyContent="center" minH="100vh">
+        {moviesError}
+      </Flex>
+    );
+  }
+
+  return (
+    <>
+      <Heading textAlign="center" size="xl" mb={4}>
+        Trending Movies
+      </Heading>
+      {content}
     </>
   );
 }
